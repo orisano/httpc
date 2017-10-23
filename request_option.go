1832ce0db59543d826aae781ad1f59e0da720d40
Nao YONASHIRO <owan.orisano@gmail.com>
2017-10-24 01:02:47 +0900

    refactor: rename prepare_option to request_option

---
+++ request_option.go
@@ -0,0 +1,118 @@
+package httpc
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type RequestOptions struct {
+	Body    io.Reader
+	Header  http.Header
+	Queries url.Values
+
+	EnforceContentLength bool
+}
+
+type RequestOption func(*RequestOptions) error
+
+func ComposeRequestOption(opts ...RequestOption) RequestOption {
+	return func(o *RequestOptions) error {
+		for _, opt := range opts {
+			if err := opt(o); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func ApplyRequestOption(o *RequestOptions, opts ...RequestOption) error {
+	return ComposeRequestOption(opts...)(o)
+}
+
+func WithBody(body io.Reader) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Body = body
+		return nil
+	}
+}
+
+func WithBinary(bin io.Reader) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header.Add("Content-Type", "application/octet-stream")
+		o.Body = bin
+		return nil
+	}
+}
+
+func WithForm(params url.Values) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		o.Body = strings.NewReader(params.Encode())
+		return nil
+	}
+}
+
+func WithJSON(data interface{}) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header.Set("Content-Type", "application/json")
+		r, w := io.Pipe()
+		go func() {
+			defer w.Close()
+			json.NewEncoder(w).Encode(data)
+		}()
+		o.Body = r
+		return nil
+	}
+}
+
+func WithXML(data interface{}) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header.Set("Content-Type", "application/xml; charset=\"UTF-8\"")
+		r, w := io.Pipe()
+		go func() {
+			defer w.Close()
+			w.Write([]byte(xml.Header))
+			xml.NewEncoder(w).Encode(data)
+		}()
+		o.Body = r
+		return nil
+	}
+}
+
+func AddHeaderField(name, value string) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header.Add(name, value)
+		return nil
+	}
+}
+
+func WithHeader(header http.Header) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Header = header
+		return nil
+	}
+}
+
+func AddQuery(key, value string) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Queries.Add(key, value)
+		return nil
+	}
+}
+
+func WithQueries(queries url.Values) RequestOption {
+	return func(o *RequestOptions) error {
+		o.Queries = queries
+		return nil
+	}
+}
+
+func EnforceContentLength(o *RequestOptions) error {
+	o.EnforceContentLength = true
+	return nil
+}
