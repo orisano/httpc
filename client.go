8e9eed4ac75db85df63ba4610a1d5acdc151cf6c
Nao YONASHIRO <owan.orisano@gmail.com>
2017-10-30 02:40:28 +0900

    feat: add Client struct

---
+++ client.go
@@ -0,0 +1,93 @@
+package httpc
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+
+	"github.com/pkg/errors"
+)
+
+type Client struct {
+	baseURL *url.URL
+	header  http.Header
+}
+
+func NewClient(rawurl string, header http.Header) (*Client, error) {
+	u, err := url.ParseRequestURI(rawurl)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse")
+	}
+	return &Client{
+		baseURL: u,
+		header:  header,
+	}, nil
+}
+
+func (c *Client) NewRequest(ctx context.Context, method, spath string, opts ...RequestOption) (*http.Request, error) {
+	if ctx == nil {
+		return nil, errors.New("missing ctx")
+	}
+	if len(method) == 0 {
+		return nil, errors.New("missing method")
+	}
+
+	u := *c.baseURL
+	if len(spath) > 0 {
+		u.Path = path.Join(u.Path, spath)
+	}
+
+	h := cloneHeader(c.header)
+	options := &RequestOptions{
+		Header:  h,
+		Queries: u.Query(),
+	}
+
+	if err := ApplyRequestOption(options, opts...); err != nil {
+		return nil, errors.Wrap(err, "failed to apply option")
+	}
+
+	u.RawQuery = options.Queries.Encode()
+
+	contentLength := int64(0)
+	if options.EnforceContentLength && options.Body != nil {
+		type sizer interface {
+			Size() int64
+		}
+		if s, ok := options.Body.(sizer); ok {
+			contentLength = s.Size()
+		} else {
+			var b bytes.Buffer
+			n, err := io.Copy(&b, options.Body)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to read body")
+			}
+			contentLength = n
+			options.Body = &b
+		}
+	}
+	req, err := http.NewRequest(method, u.String(), options.Body)
+	if err != nil {
+		return nil, err
+	}
+	if options.EnforceContentLength {
+		req.ContentLength = contentLength
+	}
+	req.Header = options.Header
+	req = req.WithContext(ctx)
+
+	return req, nil
+}
+
+func cloneHeader(h http.Header) http.Header {
+	h2 := make(http.Header, len(h))
+	for k, vv := range h {
+		vv2 := make([]string, len(vv))
+		copy(vv2, vv)
+		h2[k] = vv2
+	}
+	return h2
+}
--- request.go
+++ request.go
@@ -1,79 +1,14 @@
 package httpc
 
 import (
-	"bytes"
 	"context"
-	"io"
 	"net/http"
-	"net/url"
-
-	"github.com/pkg/errors"
 )
 
 func NewRequest(ctx context.Context, method, rawurl string, opts ...RequestOption) (*http.Request, error) {
-	if ctx == nil {
-		return nil, errors.New("missing ctx")
-	}
-	if len(method) == 0 {
-		return nil, errors.New("missing method")
-	}
-	if len(rawurl) == 0 {
-		return nil, errors.New("missing rawurl")
-	}
-
-	options := &RequestOptions{
-		Header:  make(http.Header),
-		Queries: make(url.Values),
-	}
-	if err := ApplyRequestOption(options, opts...); err != nil {
-		return nil, errors.Wrap(err, "failed to apply option")
-	}
-
-	if len(options.Queries) > 0 {
-		u, err := url.ParseRequestURI(rawurl)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to parse url: %s", rawurl)
-		}
-		q := u.Query()
-		addValues(q, options.Queries)
-		u.RawQuery = q.Encode()
-		rawurl = u.String()
-	}
-
-	contentLength := int64(0)
-	if options.EnforceContentLength && options.Body != nil {
-		type sizer interface {
-			Size() int64
-		}
-		if s, ok := options.Body.(sizer); ok {
-			contentLength = s.Size()
-		} else {
-			var b bytes.Buffer
-			n, err := io.Copy(&b, options.Body)
-			if err != nil {
-				return nil, errors.Wrap(err, "failed to read body")
-			}
-			contentLength = n
-			options.Body = &b
-		}
-	}
-	req, err := http.NewRequest(method, rawurl, options.Body)
+	c, err := NewClient(rawurl, nil)
 	if err != nil {
 		return nil, err
 	}
-	if options.EnforceContentLength {
-		req.ContentLength = contentLength
-	}
-	req.Header = options.Header
-	req = req.WithContext(ctx)
-
-	return req, nil
-}
-
-func addValues(dst, src url.Values) {
-	for k, vs := range src {
-		for _, v := range vs {
-			dst.Add(k, v)
-		}
-	}
+	return c.NewRequest(ctx, method, "", opts...)
 }
